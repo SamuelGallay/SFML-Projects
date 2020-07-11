dd174c297650eb4f70f6c178940315014d6b588c
Samuel Gallay <samuel.gallay@gmail.com>
2020-07-11 23:21:23 +0200

    Date

--- misc/upload_assets.sh
+++ misc/upload_assets.sh
@@ -19,15 +19,15 @@ JSON=$(cat <<EndOfMessage
 }
 EndOfMessage
 )
-
 curl "$AUTH" -H "Content-Type: application/json" -d "$JSON" "https://api.github.com/repos/$REPO_NAME/git/tags"
 
+# To update the release
 JSON=$(cat <<EndOfMessage
 {
   "tag_name": "auto-$SHORT_HASH",
   "target_commitish": "master",
   "name": "Latest Release",
-  "body": "",
+  "body": "Last commit : $(date)",
   "draft": false,
   "prerelease": false
 }
@@ -39,14 +39,11 @@ curl "$AUTH" -H "Content-Type: application/json" -d "$JSON" "https://api.github.
 
 # To get the asset ID from the asset NAME
 # Black magic happens here... (not mine...)
-RELEASE_INFO=$(curl -s $AUTH "https://api.github.com/repos/$REPO_NAME/releases/$RELEASE_ID")
-eval $(echo "$RELEASE_INFO" | grep -C2 "\"name\": \"$ASSET_NAME\"" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=' | sed 's/id/ASSET_ID/')
+RELEASE_INFO=$(curl -s "$AUTH" "https://api.github.com/repos/$REPO_NAME/releases/$RELEASE_ID")
+eval "$(echo "$RELEASE_INFO" | grep -C2 "\"name\": \"$ASSET_NAME\"" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=' | sed 's/id/ASSET_ID/')"
 
 # To delete the asset if already exists
-curl $AUTH -X "DELETE" "https://api.github.com/repos/$REPO_NAME/releases/assets/$ASSET_ID"
+curl "$AUTH" -X "DELETE" "https://api.github.com/repos/$REPO_NAME/releases/assets/$ASSET_ID"
 
 # To upload the new asset
-curl $AUTH --data-binary @"$2" -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$REPO_NAME/releases/$RELEASE_ID/assets?name=$ASSET_NAME"
-
-
-
+curl "$AUTH" --data-binary @"$2" -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$REPO_NAME/releases/$RELEASE_ID/assets?name=$ASSET_NAME"
